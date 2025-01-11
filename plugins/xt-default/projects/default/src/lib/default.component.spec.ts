da5be4e429e22fee1e9a3695d83790948b8dab58
Gérard Collin <gerard.collin@appliweb.net>
2025-01-11 11:46:52 +0100

    Fixed bad test config for default

--- plugins/xt-default/projects/default/src/lib/default.component.spec.ts
+++ plugins/xt-default/projects/default/src/lib/default.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DefaultComponent } from './default.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
 describe('DefaultComponent', () => {
   let component: DefaultComponent;
@@ -8,7 +9,9 @@ describe('DefaultComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DefaultComponent]
+      imports: [DefaultComponent],
+      providers: [provideExperimentalZonelessChangeDetection()]
+
     })
     .compileComponents();
 
--- plugins/xt-default/projects/default/src/lib/default.service.spec.ts
+++ plugins/xt-default/projects/default/src/lib/default.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DefaultService } from './default.service';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
 describe('DefaultService', () => {
   let service: DefaultService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideExperimentalZonelessChangeDetection()]
+    });
     service = TestBed.inject(DefaultService);
   });
 

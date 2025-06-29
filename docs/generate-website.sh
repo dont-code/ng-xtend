f814688bd7f40e5278578462c494c4a463a8b384
Gérard Collin <gerard.collin@appliweb.net>
2025-06-29 09:51:08 +0200

    auto update website from Readme

--- docs/generate-website.sh
+++ docs/generate-website.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd ..
-pandoc -f markdown -t html --file-scope=true --standalone --highlight-style=espresso --metadata title-meta="Ng-xtend framework" -V maxwidth="80%" -V backgroundcolor="#000000" -V fontcolor="#FFFFFF" -V linkcolor="#FF0000" -V pagetitle="Ng-Xtend" -H docs/matomo-script.html -o docs/ng-extend-website/index.html README.md
+mkdir dist/ng-xtend-website
+pandoc -f markdown -t html --file-scope=true --standalone --highlight-style=espresso --metadata title-meta="Ng-xtend framework" -V maxwidth="80%" -V backgroundcolor="#000000" -V fontcolor="#FFFFFF" -V linkcolor="#FF0000" -V pagetitle="Ng-xtend" -H docs/matomo-script.html -o dist/ng-xtend-website/index.html README.md
 cd docs

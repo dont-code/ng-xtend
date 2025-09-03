5893f849d61813f1c2b30bd9d986966e6d7cbf78
Gérard Collin <gerard.collin@appliweb.net>
2025-09-03 15:55:07 +0200

    primeng to primeuix

--- plugins/xt-finance/projects/finance-plugin/src/app/app.config.ts
+++ plugins/xt-finance/projects/finance-plugin/src/app/app.config.ts
@@ -1,15 +1,15 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+import Aura from '@primeuix/themes/aura';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG({

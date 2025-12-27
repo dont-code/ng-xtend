85a3dbff89c327916208f6cf0d81f253ad4c389a
Gérard Collin <gerard.collin@appliweb.net>
2025-12-27 15:15:43 +0100

    Fixed finance for plugin-tester

--- plugins/xt-finance/projects/finance-plugin/src/app/app.component.ts
+++ plugins/xt-finance/projects/finance-plugin/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StoreTestHelper, XtResolverService } from 'xt-components';
 import { registerFinancePlugin } from '../../../finance/src/lib/register';
+import { DummyCurrencyComponent } from './dummy-currency/dummy-currency.component';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,19 @@ export class AppComponent {
   constructor () {
     // Let's use the Test Store Helper for Web Images
     StoreTestHelper.ensureTestProviderOnly();
+    // Add the currency plugin to allow test
+    this.resolverService.registerPlugin({
+      name:'dummy-currency',
+      components:[{
+        componentName:'DummyCurrency',
+        componentClass: DummyCurrencyComponent,
+        typesHandled:['currency']
+      }],
+      types: {
+        currency: 'string'
+      }
+    });
+
     registerFinancePlugin(this.resolverService);
   }
 

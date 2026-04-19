5964543005c107adbc596ffb6ca6ba67314d4f84
Gérard Collin <gerard.collin@appliweb.net>
2026-04-19 11:02:18 +0200

    fix: added list details flow

---
+++ plugins/xt-workflow/projects/workflow/src/lib/list-details/list-details.component.spec.ts
@@ -0,0 +1,208 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListDetailsComponent } from './list-details.component';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { registerDefaultPlugin } from 'xt-plugin-default';
+import { XtResolverService } from 'xt-components';
+import { StoreTestBed } from 'xt-store';
+import { By } from '@angular/platform-browser';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { MessageService } from 'primeng/api';
+import { DcWorkflowModel } from 'dc-workflow';
+
+describe('ListDetailsComponent', () => {
+  let component: ListDetailsComponent<any>;
+  let fixture: ComponentFixture<ListDetailsComponent<any>>;
+  let storeTestBed: StoreTestBed;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ListDetailsComponent],
+      providers: [provideZonelessChangeDetection(), provideNoopAnimations(), MessageService]
+    })
+    .compileComponents();
+
+    registerDefaultPlugin(TestBed.inject(XtResolverService));
+    storeTestBed = new StoreTestBed();
+    StoreTestBed.ensureMemoryProviderOnly();
+  });
+
+  it(
+    'should create', () => {
+    fixture = TestBed.createComponent(ListDetailsComponent);
+    fixture.componentRef.setInput("config", {
+      entity: 'Test'
+    } as DcWorkflowModel);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+
+  it('should display list of entity',async () => {
+    await storeTestBed.defineTestDataFor('Test',[{
+      _id:"1",
+      testString:'string1',
+      testBoolean: false
+    }]);
+
+    fixture = TestBed.createComponent(ListDetailsComponent);
+    fixture.componentRef.setInput("config", {
+      entity: 'Test'
+    } as DcWorkflowModel);
+
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component.entityName()).toEqual('Test');
+
+    const debugElement = fixture.debugElement.query(By.directive(ListDetailsComponent));
+    expect(debugElement.nativeElement.textContent).toContain( 'string1');
+  });
+
+/*  it('should support full lifecycle for simple values',async () => {
+    const resolver = TestBed.inject(XtResolverService);
+
+    resolver.registerTypes({
+      "NewPrice": {
+        "amount":"string",
+        "currency":"string"
+      },
+      "NewElement": {
+        "name":"string",
+        "price":"NewPrice"
+      }
+    });
+
+    const harness = await RouterTestingHarness.create();
+    component = await harness.navigateByUrl('/entity/'+'NewElement', ListDetailsComponent);
+    expect(component).toBeTruthy();
+    expect(component.entityName()).toEqual('NewElement');
+    harness.fixture.detectChanges();
+
+    // Click on new
+    //const newSpy = vi.spyOn(component, 'newEntity');
+    const btnNew = harness.fixture.debugElement.query(By.css("#btn-new"));
+    btnNew.children[0].nativeElement.click();
+    harness.fixture.detectChanges();
+    // For some reasons, we have to wait multiple times for stability
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+
+    //expect(newSpy).toHaveBeenCalledOnce();
+    const form=component.editForm().get('editor') as FormGroup;
+    // Check fields
+    expect(Object.keys(form.controls)).toEqual([
+      '_id','name','price'
+    ]);
+    expect(Object.keys((form.get('price') as FormGroup).controls)).toEqual(['amount', 'currency']);
+
+    // Now set some values in the fields
+    let nameInput=harness.fixture.debugElement.query(By.css('[name="name"]'));
+    nameInput.nativeElement.value='NewName';
+    nameInput.nativeElement.dispatchEvent(new Event('input'));
+    harness.fixture.detectChanges();
+    const amountInput = harness.fixture.debugElement.query(By.css('[name="amount"]'));
+    amountInput.nativeElement.value='12';
+    amountInput.nativeElement.dispatchEvent(new Event('input'));
+    harness.fixture.detectChanges();
+
+    expect(form.value).toEqual({
+      _id:component.selectedEntity()!._id,
+      name:'NewName',
+      price: {
+        amount:"12",
+        currency:null
+      }
+    });
+
+    //Click on save
+    //const saveSpy = vi.spyOn(component, 'save');
+    const btnSave = harness.fixture.debugElement.query(By.css("#btn-save"));
+    btnSave.children[0].nativeElement.click();
+    harness.fixture.detectChanges();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+
+    //expect(saveSpy).toHaveBeenCalledOnce();
+
+    expect (component.selectedEntity()).toEqual({
+      _id:component.selectedEntity()!._id,
+      name:'NewName',
+      price: {
+        amount:"12",
+        currency:null
+      }
+    });
+
+    // Check we are back in displaying the list
+    expect (component.viewMode()).toEqual("list");
+    let list = harness.fixture.debugElement.query(By.directive(Table));
+    let header = list.query(By.css('thead tr'));
+    expect(header.children).toHaveLength(2); // Header
+    expect(header.nativeElement.textContent).toEqual("nameprice");
+
+    let row = list.query(By.css('tbody tr'));
+    // Content
+    expect(row.nativeElement.textContent).toSatisfy((text:string)=> text.indexOf('NewName')!=-1,"No edited value displayed in the list");
+
+    row.children[0].nativeElement.click();
+    row.children[0].nativeElement.click();
+    row.children[0].nativeElement.click();
+    harness.fixture.detectChanges();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+
+    //It should have switched to edit mode
+    expect (component.viewMode()).toEqual("edit");
+    nameInput=harness.fixture.debugElement.query(By.css('[name="name"]'));
+    nameInput.nativeElement.value='NewestName';
+    nameInput.nativeElement.dispatchEvent(new Event('input'));
+    harness.fixture.detectChanges();
+
+    btnSave.children[0].nativeElement.click();
+    harness.fixture.detectChanges();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+
+
+    expect (component.selectedEntity()).toEqual({
+      _id:component.selectedEntity()!._id,
+      name:'NewestName',
+      price: {
+        amount:"12",
+        currency:null
+      }
+    });
+
+    // Check the list content has been updated as well
+    list = harness.fixture.debugElement.query(By.directive(Table));
+    row = list.query(By.css('tbody tr'));
+    expect(row.nativeElement.textContent).toSatisfy((text:string)=> text.indexOf('NewestName')!=-1,"Newest value not displayed in the list");
+
+    // Now try to delete element
+    //const deleteSpy = vi.spyOn(component, 'deleteSelected');
+    const btnDelete = harness.fixture.debugElement.query(By.css("#btn-delete"));
+    btnDelete.children[0].nativeElement.click();
+    harness.fixture.detectChanges();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+    await harness.fixture.whenStable();
+
+    //expect(deleteSpy).toHaveBeenCalledOnce();
+    list = harness.fixture.debugElement.query(By.directive(Table));
+    expect(list).toBeNull();
+    const emptyMsg = harness.fixture.debugElement.query(By.css('.list-details__state--empty'));
+    expect(emptyMsg.nativeElement.textContent.indexOf("No entity found")).not.toEqual(-1);
+
+  });*/
+
+});
---
+++ plugins/xt-workflow/projects/workflow/src/lib/list-details/list-details.component.ts
@@ -0,0 +1,194 @@
+import { ChangeDetectionStrategy, Component, computed, inject, linkedSignal, OnDestroy, signal } from '@angular/core';
+import {
+  updateFormGroupWithValue,
+  XtBaseModel,
+  XtComponentModel,
+  XtMessageHandler,
+  XtRenderComponent
+} from 'xt-components';
+import { FormBuilder, FormGroup, PristineChangeEvent, ReactiveFormsModule } from '@angular/forms';
+import { ManagedData } from 'xt-type';
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from 'primeng/tabs';
+import { Toolbar } from 'primeng/toolbar';
+import { Button } from 'primeng/button';
+import { Subscription } from 'rxjs';
+import { ProgressSpinner } from 'primeng/progressspinner';
+import { AbstractDcWorkflow } from 'dc-workflow';
+
+@Component({
+  selector: 'wfw-list-details',
+  imports: [XtRenderComponent, ReactiveFormsModule, TabPanel, TabPanels, Tab, TabList, Tabs, Toolbar, Button, ProgressSpinner],
+  providers: [],
+  templateUrl: './list-details.component.html',
+  styleUrl: './list-details.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ListDetailsComponent<T extends ManagedData> extends AbstractDcWorkflow<T> implements OnDestroy {
+  protected readonly formBuilder = inject(FormBuilder);
+  protected readonly errorHandler = inject(XtMessageHandler);
+
+  entityName = linkedSignal( () => {
+    return this.config().entity;
+  });
+
+  editForm = signal<FormGroup>(this.formBuilder.group ({ editor: this.formBuilder.group({}) }));
+
+  selectedEntity = signal<T|null>(null);
+
+  canEdit= computed(()=> {
+    if (this.selectedEntity()!=null)
+      return true;
+    return false;
+  });
+
+  viewMode = linkedSignal( () => {
+    const selection = this.selectedEntity();
+    if (selection!=null) {
+      return "edit";
+    }else return "list";
+  });
+
+  canSave=signal (false);
+
+  saving = signal (false);
+  newing = signal (false);
+  deleting = signal (false);
+  updating = signal (false);
+
+  listModel =new XtBaseModel<any>();
+
+  private subscriptions=new Subscription();
+
+  constructor() {
+    super();
+    this.fetchFromStore();
+  }
+
+  fetchFromStore () {
+    const entityName = this.entityName();
+    if (entityName!=null) {
+      try {
+        this.updating.set(true);
+        const store = this.safeFindStore();
+        store.fetchEntities().catch((error) => {
+          this.errorHandler.errorOccurred(error, "Error loading entities "+entityName);
+        }).finally(() => {
+          this.updating.set(false);
+        });//.then(() => {console.debug('Yes')}).finally(() => {console.debug('Finish')});
+      } catch (error) {
+        this.updating.set(false);
+      }
+    } else {
+      this.store = null;
+    }
+  }
+
+  listModelChanged($event : any) {
+    const newValue=$event as XtComponentModel;
+    if (newValue?.valueSelected!=null) {
+      newValue?.valueSelected.subscribe (selected => {
+        this.selectedEntity.set(selected);
+        this.updateEditForm();
+      });
+    }
+  }
+
+  updateEditForm () {
+    const entity = this.selectedEntity();
+    const form = this.formBuilder.group({}, {updateOn: 'change'});
+    if (entity!=null) {
+      updateFormGroupWithValue(form, entity, this.entityName(), this.resolver.typeResolver);
+    }
+    this.listenToFormEvent (form);
+    this.editForm.set( this.formBuilder.group ({ editor: form }));
+  }
+
+  async save() {
+    const toSave = this.editForm().value.editor as T;
+    if (toSave==null) {
+      throw new Error ("Trying to save an entity that is not bind to form");
+    }
+
+    try {
+      this.saving.set(true);
+      const savedValue = await this.safeFindStore().storeEntity (toSave);
+      this.selectedEntity.set(savedValue);
+      this.updateEditForm();
+      this.canSave.set(false);
+      this.viewMode.set("list");
+    } catch (error) {
+      this.errorHandler.errorOccurred(error, "Error saving entity with id "+ toSave._id);
+    } finally {
+      this.saving.set(false);
+    }
+  }
+
+  private listenToFormEvent(form: FormGroup) {
+    this.subscriptions.unsubscribe();
+    this.subscriptions = new Subscription();
+    this.subscriptions.add(form.events.subscribe(event => {
+      const pristine = (event as PristineChangeEvent).pristine??true;
+      if (!pristine) {
+        this.canSave.set(true);
+      }
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  async deleteSelected() {
+    const toTrash = this.selectedEntity();
+    if (toTrash==null) {
+      throw new Error ("Select an entity before clicking on delete");
+    }
+    let deleted=false;
+    if (toTrash._id!=null) {
+      try {
+        this.deleting.set(true);
+        deleted = await this.safeFindStore().deleteEntity (toTrash._id);
+      } catch (error) {
+        this.errorHandler.errorOccurred(error, "Deleting entity with id "+toTrash._id);
+      } finally {
+        this.deleting.set(false);
+      }
+    } else {
+      deleted=true;
+    }
+    if (deleted) {
+      this.selectedEntity.set(null);
+      this.viewMode.set("list");
+      this.updateEditForm();
+    }
+    }
+
+  async newEntity() {
+    try {
+      this.newing.set(true);
+      const newOne = await this.safeFindStore().storeEntity({} as T);
+      this.selectedEntity.set(newOne);
+      this.updateEditForm();
+      this.viewMode.set("edit");
+
+    } catch (error) {
+      this.errorHandler.errorOccurred(error, "Error creating and storing new Entity")
+    } finally {
+      this.newing.set(false);
+    }
+  }
+
+  canReload = computed(() => {
+    if (this.store!=null) {
+      return ! this.store.loading();
+    }else {
+      return false;
+    }
+
+  })
+
+  reloadList() {
+    this.fetchFromStore();
+  }
+
+}

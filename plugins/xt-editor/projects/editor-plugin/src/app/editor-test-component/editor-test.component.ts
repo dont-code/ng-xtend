8f6be665d1075e74e54b2e357e32fe1dee7ebc33
Gérard Collin <gerard.collin@appliweb.net>
2026-09-19 11:15:07 +0200

    fix: editor with form

--- plugins/xt-editor/projects/editor-plugin/src/app/editor-test-component/editor-test.component.ts
+++ plugins/xt-editor/projects/editor-plugin/src/app/editor-test-component/editor-test.component.ts
@@ -1,24 +1,27 @@
-import { Component, inject, OnDestroy, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { XtBaseContext, XtContext, XtResolverService } from 'xt-components';
 import { EditorComponent } from '../../../../editor/src/lib/editor/editor.component';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-editor-test',
   imports: [
     FormsModule,
     ReactiveFormsModule,
-    EditorComponent
+    EditorComponent,
+    JsonPipe
   ],
   templateUrl: './editor-test.component.html',
   styleUrl: './editor-test.component.css'
 })
-export class EditorTestComponent implements OnDestroy {
+export class EditorTestComponent implements OnInit,OnDestroy {
 
   protected builder = inject(FormBuilder);
 
   protected resolver = inject (XtResolverService);
+  protected readonly formValue = signal<any>(null);
 
   protected subscriptions= new Subscription();
   protected readonly simpleForm = signal<FormGroup>(this.builder.group({
@@ -29,6 +32,12 @@ export class EditorTestComponent implements OnDestroy {
 
   }
 
+  ngOnInit() {
+    this.subscriptions.add(this.simpleForm().valueChanges.subscribe({
+      next: (value)=>this.formValue.set(value.simpleText)
+    }));
+  }
+
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
   }
@@ -37,4 +46,5 @@ export class EditorTestComponent implements OnDestroy {
     const ret= new XtBaseContext('FULL_EDITABLE', "simpleText",this.simpleForm());
     return ret;
   }
+
 }
--- plugins/xt-editor/projects/editor/src/lib/editor/editor.component.ts
+++ plugins/xt-editor/projects/editor/src/lib/editor/editor.component.ts
@@ -1,17 +1,9 @@
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  Injector,
-  input,
-  viewChild
-} from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, input, viewChild } from '@angular/core';
 import { XtContext, XtSimpleComponent } from 'xt-components';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { EditorState } from 'prosemirror-state';
+import { EditorState, Transaction } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
-import { DOMParser, Schema } from 'prosemirror-model';
+import { DOMParser,Node, Schema } from 'prosemirror-model';
 import { schema } from 'prosemirror-schema-basic';
 import { addListNodes } from 'prosemirror-schema-list';
 import { basicSetup } from '../prose-mirror/basic-setup';
@@ -32,9 +24,8 @@ export class EditorComponent extends XtSimpleComponent<any> implements AfterView
   protected editor = viewChild.required<ElementRef<HTMLDivElement>>('editor');
   protected editorContent = viewChild.required<ElementRef<HTMLDivElement>>('editorContent');
 
-  constructor(private injector: Injector, private elementRef: ElementRef<HTMLElement>) {
-    super();
-  }
+  protected sampleJson={ "type": "doc", "content": [ { "type": "heading", "attrs": { "level": 1 }, "content": [ { "type": "text", "text": "Example Text" } ] }, { "type": "paragraph", "content": [ { "type": "text", "text": "s it working ?" } ] } ] };
+  protected emptyJson={ "type": "doc", "content": [ { "type": "paragraph"} ] };
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
   mySchema: Schema = new Schema({
@@ -48,17 +39,51 @@ export class EditorComponent extends XtSimpleComponent<any> implements AfterView
     const editor=this.editor();
     const editorContent=this.editorContent();
     if( (editor!=null) && (editorContent!=null)) {
-      const doc= DOMParser.fromSchema(this.mySchema).parse(editorContent.nativeElement);
+      const value=this.context().formControlValue();
+
+      const doc = this.toProseMirrorDoc (value);
+      //const doc= DOMParser.fromSchema(this.mySchema).parse(editorContent.nativeElement);
       this.view = new EditorView(editor.nativeElement, {
         state: EditorState.create({
           doc: doc,
           plugins: basicSetup({schema: this.mySchema})
-        })
+        }),
+        dispatchTransaction: this.handleTransactions.bind(this)
       })
     }
   /*  this.editor.valueChanges.pipe(takeUntil(this.unsubscribe)).subscribe((jsonDoc) => {
       this.handleChange(jsonDoc);
     });*/
   }
+
+  protected handleTransactions(tr: Transaction): void {
+    if (this.view!=null) {
+      const state = this.view.state.apply(tr);
+      this.view.updateState(state);
+
+      if (tr.docChanged) {
+        const json = state.doc.toJSON();
+        this.context().setFormValue(json, true);
+      }
+    } else {
+      console.error("Editor: View transaction received while no view defined.",tr.doc.toJSON());
+    }
+  }
+
+
+  private toProseMirrorDoc(value: any): Node {
+    if (value == null) {
+      return this.mySchema.nodeFromJSON ( this.emptyJson);
+    }
+    if (typeof value === 'string') {
+      // This is just text
+      return this.mySchema.nodeFromJSON(this.textJson(value));
+    }
+    return this.mySchema.nodeFromJSON (value);
+  }
+
+  protected textJson (text:string){
+    return { "type": "doc", "content": [ { "type": "paragraph", "content": [ { "type": "text", "text": text } ] } ] }
+  };
 
 }
